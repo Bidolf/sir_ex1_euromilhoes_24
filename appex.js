33ca9d92112e916ac203d37c93350caaa311cb54
Diogo Marques <dimarques@ipvc.pt>
2024-10-24 11:44:34 +0100

    trim

--- appex.js
+++
@@ -1,6 +0,0 @@
-//express js
-const express = require('express');
-const app = express
-
-app.get('/hello',(req,res) => res.send("Hello Express"));
-app.listen(3000,() => console.log('Server with expressjs is running'));
